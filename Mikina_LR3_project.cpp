dc79414760489e1ab31b2402a10cfaaab2c6276a
mikinaksu <mikinaksu8@gmail.com>
2025-04-01 09:25:15 +0300

    change code

--- Mikina_LR3_project.cpp
+++ Mikina_LR3_project.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
 
-double EnterNum()
+double EnterA()
+{
+    //разрабатывается Ивановым П. - ветка branch_fun_1
+}
+double EnterB()
+{
+    //разрабатывается Ивановым П. - ветка branch_fun_1
+}
+double EnterC()
 {
     //разрабатывается Ивановым П. - ветка branch_fun_1
 }
@@ -20,16 +28,28 @@ int main() {
     double EnterNum;
 
     do {
-        cout << "Task 1. Найти остаток от деления разности чисел A и B на число C" << endl;
-        cout << "Task 2. Найти целую часть от деления числа A на сумму чисел B и C" << endl;
-        cout << "Пожалуйста, введите число от 1 до 2 или 0, если хотите закрыть приложение: ";
+        cout << "Task 1. Ввести целое число A" << endl;
+        cout << "Task 2. Ввести целое число B" << endl;
+        cout << "Task 3. Ввести целое число C" << endl;
+        cout << "Task 4. Найти остаток от деления разности чисел A и B на число C" << endl;
+        cout << "Task 5. Найти целую часть от деления числа A на сумму чисел B и C" << endl;
+        cout << "Пожалуйста, введите число 0, если хотите закрыть приложение ";
         cin >> choice;
 
         switch (choice) {
             case 1:
-                cout << "Остаток от деления (A - B) % C = " << CalcOstatok(A, B, C) << endl;
+                cout << "Введите ЦЕЛОЕ число A" << EnterA << endl;
                 break;
             case 2:
+                cout << "Введите ЦЕЛОЕ число B" << EnterB << endl;
+                break;
+            case 3:
+                cout << "Введите ЦЕЛОЕ число C" << EnterC << endl;
+                break;
+            case 4:
+                cout << "Остаток от деления (A - B) % C = " << CalcOstatok(A, B, C) << endl;
+                break;
+            case 5:
                 cout << "Целая часть от деления A / (B + C) = " << CalcCelayaPart(A, B, C) << endl;
                 break;
             case 0:
